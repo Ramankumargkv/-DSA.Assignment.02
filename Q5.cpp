30d2e9f63e3b1bb5f50aa28cec6f7cc1c0b95624
Raman8651 <121950276+Ramankumargkv@users.noreply.github.com>
2024-03-11 15:37:44 +0530

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,12 @@
+// Q5 WAP to find the difference between ASCII of two characters ,take them as input .
+#include<iostream>
+using namespace std ;
+int main()
+{
+    char cha1,cha2;
+    cout<<"Enetr the first character = ";
+    cin>>cha1;
+    cout<<"Enter the second character = ";
+    cin>>cha2;
+    cout<<"diffrent b/w to characters is = "<<cha1-cha2;
+}
